05e11aa31d9260c886339ebd1abb8e49f3d6edb8
Ankit Gupta <04.ankitgupta@gmail.com>
2025-10-06 00:14:25 +0530

    m

--- src/modules/menuCategory/menuCategory.controller.ts
+++ src/modules/menuCategory/menuCategory.controller.ts
@@ -4,6 +4,7 @@ import { asyncHandler } from "../../utils/asyncHandler.js";
 import * as menuCategoryService from "./menuCategory.service.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { type AuthRequest } from "../../middlewares/auth.middleware.js";
+import { ApiError } from "../../utils/ApiError.js";
 
 export const getAllMenuCategoriesController = asyncHandler(
   async (req: AuthRequest, res: Response) => {
@@ -14,5 +15,68 @@ export const getAllMenuCategoriesController = asyncHandler(
     res
       .status(httpStatus.OK)
       .json(new ApiResponse(httpStatus.OK, menuCategories));
+  }
+);
+
+export const createMenuCategoryController = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const restaurantId = req.user?.restaurantId;
+    const newCategory = await menuCategoryService.createMenuCategory(
+      req.body,
+      restaurantId!
+    );
+    res
+      .status(httpStatus.CREATED)
+      .json(
+        new ApiResponse(
+          httpStatus.CREATED,
+          newCategory,
+          "Category created successfully"
+        )
+      );
+  }
+);
+
+export const updateMenuCategoryController = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const restaurantId = req.user?.restaurantId;
+    const { categoryId } = req.params;
+
+    if (!categoryId) {
+      throw new ApiError(httpStatus.BAD_REQUEST, "Category ID is required");
+    }
+
+    const updatedCategory = await menuCategoryService.updateMenuCategory(
+      categoryId,
+      req.body,
+      restaurantId!
+    );
+    res
+      .status(httpStatus.OK)
+      .json(
+        new ApiResponse(
+          httpStatus.OK,
+          updatedCategory,
+          "Category updated successfully"
+        )
+      );
+  }
+);
+
+export const deleteMenuCategoryController = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const restaurantId = req.user?.restaurantId;
+    const { categoryId } = req.params;
+
+    if (!categoryId) {
+      throw new ApiError(httpStatus.BAD_REQUEST, "Category ID is required");
+    }
+
+    await menuCategoryService.deleteMenuCategory(categoryId, restaurantId!);
+    res
+      .status(httpStatus.OK)
+      .json(
+        new ApiResponse(httpStatus.OK, {}, "Category deleted successfully")
+      );
   }
 );
